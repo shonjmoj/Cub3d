8a67aaf0371fdb421ff36a3a133d05130c5d963f
shonjmoj <midoounejjart@gmail.com>
2022-10-29 12:20:28 +0100

    fix : norminette

--- cub3d.h
+++ cub3d.h
@@ -6,17 +6,21 @@
 /*   By: elounejj <elounejj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 12:08:24 by elounejj          #+#    #+#             */
-/*   Updated: 2022/10/23 12:17:23 by elounejj         ###   ########.fr       */
+/*   Updated: 2022/10/29 12:16:35 by elounejj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include "Libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include "Libft/libft.h"
+
+# define BUFFER_SIZE 1000
+
+char	*get_next_line(int fd);
 
 #endif
--- parsing.c
+++ parsing.c
@@ -1,62 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elounejj <elounejj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/29 11:53:48 by elounejj          #+#    #+#             */
+/*   Updated: 2022/10/29 11:57:20 by elounejj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-char *get_ext(char *filename)
+char	*get_ext(char *filename)
 {
-    int i;
-    int j;
-    char *ext;
+	int		i;
+	int		j;
+	char	*ext;
 
-    i = 0;
-    ext = NULL;
-    j = ft_strlen(filename);
-    while (filename[j - 1] >= 0)
-    {
-        if (filename[j - 1] == '.')
-        {
-            j--;
-            break;
-        }
-        j--;
-    }
-    ext = malloc(sizeof(char) * (j + 2));
-    while (filename[i + j])
-    {
-        ext[i] = filename[i + j];
-        i++;
-    }
-    ext[i] = '\0';
-    return (ext);
+	i = 0;
+	ext = NULL;
+	j = ft_strlen(filename);
+	while (filename[j - 1] >= 0)
+	{
+		if (filename[j - 1] == '.')
+		{
+			j--;
+			break ;
+		}
+		j--;
+	}
+	ext = malloc(sizeof(char) * (j + 2));
+	while (filename[i + j])
+	{
+		ext[i] = filename[i + j];
+		i++;
+	}
+	ext[i] = '\0';
+	return (ext);
 }
 
-int check_map_ext(char *filname)
+int	check_map_ext(char *filname)
 {
-    char *ext;
+	char	*ext;
 
-    if (ft_strnstr(filname, ".cub", ft_strlen(filname)) == NULL)
-        return (0);
-    ext = get_ext(filname);
-    if (ft_strncmp(".cub", ext, ft_strlen(".cub")) != 0 || *filname == '.')
-    {
-        free(ext);
-        return (0);
-    }
-    free(ext);
-    return (1);
-}
-
-int main(int argc, char **argv)
-{
-    if (argc == 1)
-        return 1;
-    if (check_map_ext(argv[1]))
-    {
-        printf("OK\n");
-        return (0);
-    }
-    else
-    {
-        printf("KO\n");
-        return 1;
-    }
-    return (0);
+	if (ft_strnstr(filname, ".cub", ft_strlen(filname)) == NULL)
+		return (0);
+	ext = get_ext(filname);
+	if (ft_strncmp(".cub", ext, ft_strlen(".cub")) != 0 || *filname == '.')
+	{
+		free(ext);
+		return (0);
+	}
+	free(ext);
+	return (1);
 }
