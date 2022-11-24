533dc43599c9e8d6c4016284085a8544826ebc97
shonjmoj <midoounejjart@gmail.com>
2022-11-24 15:32:58 +0100

    fix : error messages

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: elounejj <elounejj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 14:54:01 by elounejj          #+#    #+#             */
-/*   Updated: 2022/11/24 15:28:22 by elounejj         ###   ########.fr       */
+/*   Updated: 2022/11/24 15:32:33 by elounejj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 		print_error("Invalid arguments !");
 	if (!check_map_ext(argv[1]))
-		print_error("Invalid map !");
+		print_error("Invalid map file !");
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		print_error("Cannot open this file !");
 	if (!valid_parsing(&map, fd))
-		print_error("Invalid Map !");
+		print_error("Invalid map config !");
 	return (0);
 }
